28e8eb989e9c9755f34c65eaf909ca64377f569b
AshwiniNakod <nakodashwini@gmail.com>
2022-11-01 19:38:24 +0530

    insert One

--- routers/dbQuery.js
+++ routers/dbQuery.js
@@ -1,16 +1,17 @@
+import { ObjectId } from "mongodb";
 import { client } from "../index.js";
 
 export async function updateMovieById(id, data) {
-    return await client.db("ashwini").collection("movies").updateOne({ id: id }, { $set: data });
+    return await client.db("ashwini").collection("movies").updateOne({ _id: ObjectId(id) }, { $set: data });
 }
 export async function deleteMovieById(id) {
-    return await client.db("ashwini").collection("movies").deleteOne({ id: id });
+    return await client.db("ashwini").collection("movies").deleteOne({ _id: ObjectId(id) });
 }
 export async function getMovieByID(id) {
-    return await client.db("ashwini").collection("movies").findOne({ id: id });
+    return await client.db("ashwini").collection("movies").findOne({ _id:ObjectId(id) });
 }
 export async function createMovies(data) {
-    return await client.db("ashwini").collection("movies").insertMany(data);
+    return await client.db("ashwini").collection("movies").insertOne(data);
 }
 export async function getAllMovies(req) {
     return await client.db("ashwini").collection("movies").find(req.query).toArray();
